bce1048166c956af5fa6aa44c6056d27b64d570b
Elissa <welissa@seas.upenn.edu>
2014-02-15 19:29:03 -0500

    added mean_test

--- mean_test.m
+++ mean_test.m
@@ -1,30 +1,37 @@
 
 %% Initialization
 SamplesPerFrame = 2048;
-FReader = dsp.AudioFileReader('clips/ad1.wav','SamplesPerFrame',SamplesPerFrame);
+FReader = dsp.AudioFileReader('clips/cb2/ad-s3e7.wav','SamplesPerFrame',SamplesPerFrame, ...
+    'PlayCount',1);
 Fs = FReader.SampleRate;
 
 TimeScope = dsp.TimeScope('SampleRate',Fs,'BufferLength',4*Fs,...
-	 'TimeSpan',300,'YLimits',[-0.5 0.5],'ShowGrid',true);
+	 'TimeSpan',60,'YLimits',[-0.5 0.5],'ShowGrid',true);
 
-TimeScopeOut = dsp.TimeScope('SampleRate',Fs,'BufferLength',4*Fs,...
-	 'TimeSpan',300,'YLimits',[-0.5 0.5],'ShowGrid',true, 'Name', 'Output Time Scope');
+%TimeScopeOut = dsp.TimeScope('SampleRate',Fs,'BufferLength',4*Fs,...
+%	 'TimeSpan',300,'YLimits',[-0.5 0.5],'ShowGrid',true, 'Name', 'Output Time Scope');
 
 Player = dsp.AudioPlayer('SampleRate',Fs);
-
-FWriter = dsp.AudioFileWriter('SampleRate',Fs,...
-	 'FileFormat','M4A','Filename', 'output.M4A');
  
-%Meaner = dsp.Mean('RunningMean', true);
 Meaner = dsp.Mean();
+
+%% Variables to be tuned
+max_comm_length = 30; % Maximum length of a single commercial
+max_comm_block_length = 180; % Maximum length of a block of commercials
+threshold = 1; % Ignore detected silences if it's been < threshold
+               % time since last silence
+
+%% Initialize features for figuring stuff out
+in_commercial = false;
+last_toggle = 0;
+last_silence = 0;
+time_since_start = 0; % Should we have this or just use toc?
 
 %% Stream
 tic;
 i=0;
-in_commercial = false;
-time_last_toggle = 0;
 
-while toc < 300
+while ~isDone(FReader)
 	 % Read frame from file
 	 audioIn = step(FReader);
      
@@ -32,30 +39,52 @@ while toc < 300
 	 audioOut = 0.8*audioIn;
 
 	 % View audio waveform
-	 step(TimeScope,[audioIn]);
+	 step(TimeScope,audioIn);
 
 	 % Play resulting audio, if we're not in a commercial
-     if (in_commercial==false)
+     if (in_commercial==true)
         audioOut = zeros(size(audioOut)); 
      end
      step(Player,audioOut);
 
     % View waveform of output sound
-     step(TimeScopeOut, audioOut);
-
-	 % Write resulting audio
-	 %step(FWriter,audioOut);
+    % step(TimeScopeOut, audioOut);
      
      % Calculate mean
      m = step(Meaner, audioIn);
      m = sum(m,2);
+     
+     % If we're sure we're not in a commercial, fix it!
+     % Basically, if there hasn't been a silence in a long time,
+     % we must be in the show.
+     if (in_commercial)
+         if ((toc - last_silence) > max_comm_length)
+             t = toc - last_silence
+            in_commercial = false
+         end
+     end
+     
      % check for silence
      if (m==0)
-         % if we've found a silence, see if it's been enough time
-         if (toc - time_last_toggle > 30)
-         in_commercial = ~in_commercial
-         time_last_toggle = toc;
+         
+         % If we just saw a silence, this one is meaningless
+         if ((toc - last_silence) < threshold)
+             % Set time since last silence to now
+            last_silence = toc; 
+            % Ignore this silence
+            'ignoring silence'
+            continue;
          end
+         
+         % if we've found a silence, see if it's been enough time
+         if (toc - last_toggle > max_comm_block_length)
+            in_commercial = true
+            last_toggle = toc;
+            % last_silence = toc;   
+         end
+         
+         % Set current time to be the last silence found
+         last_silence = toc;
      end
 
 end
@@ -66,4 +95,3 @@ s = [];
 release(TimeScope)
 s.TimeScope = TimeScope;
 release(Player)
-release(FWriter)
